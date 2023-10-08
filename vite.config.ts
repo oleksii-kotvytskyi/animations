c9fb1a5bf4505c3cee36cb52a6698fe581a62a5a
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2023-10-08 16:40:28 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  build: {
-    modulePreload: true,
-    sourcemap: true,
-    manifest: true,
-  },
   base: "/animations1/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
   },
 });
