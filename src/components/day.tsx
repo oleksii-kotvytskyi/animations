25dcfe71589a66f4c6e050b2b4c1e44b21b46a6e
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2023-10-08 20:25:22 +0300

    check if gh action will push changes to server

--- src/components/day.tsx
+++ src/components/day.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 export const Day = () => {
   return (
     <div className="flex items-center justify-center h-screen w-full">
-      Day ...
       <Outlet />
     </div>
   );
